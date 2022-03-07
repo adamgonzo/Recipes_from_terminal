f95606a8f715c0a315394e76d7d23c19fd4ec899
adamgonzo <adam.gonzo@outlook.com>
2022-03-06 23:07:38 -0800

    Main file for recipe information

---
+++ main.py
@@ -0,0 +1,38 @@
+from home_cook_class.home_cook import HomeCook
+from clear import clear
+
+if __name__ == '__main__':
+    from art import tprint
+    tprint("Recipes For You!")
+
+    while True:
+        name_of_food = input("Please enter type of food you would like to make (ex: kale, shrimp, salmon, etc): ")
+        clear()
+        cook = HomeCook(name_of_food)
+        data_for_food = cook.food.grab_food_data()
+        choices = {}
+        counter = 1
+        for key, value in data_for_food.items():
+            choices[counter] = [key, value]
+            counter += 1
+        print("Here is the top 10 recipes for {name}:".format(name=name_of_food))
+        for i, key in enumerate(data_for_food.keys()):
+            print('\t', str(i+1) + ': ' + key)
+
+        choice = int(input("Please enter choice 1-10: "))
+        clear()
+        cook.grab_food_ingredients_and_steps(choices[choice][1])
+        end = input("\nPlease enter (y or Y) to exit or (n or N) to try another reciepe: " )
+        if end.lower() == 'y':
+            exit()
+        else:
+            clear()
+            continue
+        
+
+
+
+
+
+
+
