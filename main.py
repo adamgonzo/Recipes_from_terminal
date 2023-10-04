3c2c0efd2fb6bb6ad694412fea28cfc0d35fdea3
Adam Gonzalez <adam.gonz@outlook.com>
2023-10-03 19:52:52 -0700

    fixed a couple things
    added a back option and created an option if there was no recipe to ask the user if he wants
    to pick something else

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ if __name__ == '__main__':
     terminal_txt = pyfiglet.figlet_format(txt, font="doom", justify="center")
     print(terminal_txt)
 
+
+
     while True:
-        name_of_food = input("Please enter type of food you would like to make (ex: kale, shrimp, salmon, etc): ")
+        name_of_food = input("Please enter ingredients for the food you would like to make if more than one ingredient seperate by comma(ex: kale, shrimp, salmon, etc)\n\n: ").strip(' ')
         clear()
+
         cook = HomeCook(name_of_food)
+
         data_for_food = cook.food.grab_food_data()
+
         choices = {}
         counter = 1
 
@@ -21,12 +26,43 @@ if __name__ == '__main__':
             choices[counter] = [key, value]
             counter += 1
 
-        print("Here is the top 10 recipes for {name}:".format(name=name_of_food))
+        if len(data_for_food) == 0:
+            print("No recipes found please enter different ingredients")
+            done = False
+            while True:
+                choice = input("Would you like to look up another recipe Yes(y or Y) No(n or N) ?")
+
+                if choice.lower() == 'y':
+                    clear()
+                    break
+
+                elif choice.lower() == 'n':
+                    clear()
+                    print("Have a good day!")
+                    done = True
+                    break
+                else:
+                    print("Please enter a choice that is available")
+                    continue
+
+            if done == True:
+                break
+            elif (len(data_for_food) == 0 and done == False):
+                continue
+
+        print("Here is the top {total} recipes for {name}:".format(name=name_of_food, total=len(data_for_food)))
         for i, key in enumerate(data_for_food.keys()):
             print('\t', str(i+1) + ': ' + key)
 
-        choice = int(input("Please enter choice 1-10: "))
+        choice = input("\nPlease enter choice 1-10 or (b or B) for Back to try something else: ")
+        if choice.isdigit():
+            choice = int(choice)
+        else:
+            if choice.lower() == 'b':
+                clear()
+                continue
         clear()
+
         cook.grab_food_ingredients_and_steps(choices[choice][1], choices[choice][0])
 
         
@@ -37,7 +73,6 @@ if __name__ == '__main__':
             clear()
             continue
         else:
-            #added pdf
             pdf = FPDF()
             pdf.add_page()
             pdf.set_font("Arial", size=15)
@@ -48,11 +83,5 @@ if __name__ == '__main__':
             os.remove('recipe.txt')
             clear()
             break
-        
-
-
-
-
-
 
 
